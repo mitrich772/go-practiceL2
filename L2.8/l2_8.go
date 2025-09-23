bc1e82662484b3b242d0da0a64fbcfe416c74c71
mitrich772 <mitrichdmitrich772@gmail.com>
2025-09-23 17:27:35 +0300

    Добавил решение L2.8

---
+++ L2.8/l2_8.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+func main (){
+	options := ntp.QueryOptions{ Timeout: 10*time.Second}
+	response, err := ntp.QueryWithOptions("0.beevik-ntp.pool.ntp.org", options)
+	if err != nil {
+		log.Printf("Ошибка: %s", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Текущее время: %s\n", response.Time)
+}
