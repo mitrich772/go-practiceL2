3c7b9628dc3a06a2d0a0380d829004c449f251c2
mitrich772 <mitrichdmitrich772@gmail.com>
2025-09-30 20:02:57 +0300

    добавил решение 2.10

--- L2.10/l2_10.go
+++ L2.10/l2_10.go
@@ -1,40 +1,119 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
-	"io"
+	"log"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	args := os.Args[1:]
-	fmt.Println("Args:")
-	for _, arg := range args {
-		fmt.Print(arg + " ")
-	}
-	fmt.Println()
-
-	// if _, err := os.Create("data.txt"); err != nil {
-	// 	fmt.Printf("file not created: %s", err)
-	// 	os.Exit(1)
-	// }
-	file, err := os.Open("data.txt")
+// Читает все непустые строки с файла
+func readLines(filename string) ([]string, error) {
+	file, err := os.Open(filename)
 	if err != nil {
-		fmt.Print(err)
-		os.Exit(1)
+		return nil, err
 	}
 	defer file.Close()
-	data := make([]byte, 64)
-	for {
-		n, err := file.Read(data)
-		if err == io.EOF {
-			break
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.TrimSpace(line) != "" { // пропускаем пустые строки
+			lines = append(lines, line)
 		}
-		if err != nil {
-			fmt.Println("Ошибка чтения:", err)
-			break
-		}
-		fmt.Print(string(data[:n]))
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return lines, nil
+}
+
+// Удаляет дубликаты но только в отсортированном массиве где дубликаты стоят рядом
+func removeDuplicatesSorted(lines []string) []string {
+	if len(lines) == 0 {
+		return lines
 	}
 
+	uniq := []string{lines[0]}
+	prev := lines[0]
+
+	for _, line := range lines[1:] {
+		if line != prev {
+			uniq = append(uniq, line)
+			prev = line
+		}
+	}
+
+	return uniq
+}
+
+func main() {
+	var column int
+	var numeric bool
+	var reverse bool
+	var unique bool
+
+	flag.IntVar(&column, "k", 1, "column for sort")
+	flag.BoolVar(&numeric, "n", false, "sort string as number")
+	flag.BoolVar(&reverse, "r", false, "reverse sort")
+	flag.BoolVar(&unique, "u", false, "only unique values")
+	flag.Parse()
+
+	fmt.Printf("column %d, numeric %t, reverse %t, unique %t\n", column, numeric, reverse, unique)
+
+	lines, err := readLines("data.txt")
+	if err != nil {
+		log.Println(err)
+	}
+
+	slices.SortFunc(lines, func(a, b string) int { // правило сортировки
+		fa := strings.Split(a, "\t")
+		fb := strings.Split(b, "\t")
+
+		if len(fa) < column || len(fb) < column {
+			log.Printf("строка имеет меньше столбцов, чем k=%d: '%v' / '%v'\n", column, fa, fb)
+			os.Exit(1)
+		}
+
+		va := fa[column-1]
+		vb := fb[column-1]
+
+		if numeric {
+			aInt, _ := strconv.Atoi(va)
+			bInt, _ := strconv.Atoi(vb)
+			if reverse {
+				if aInt < bInt {
+					return 1
+				} else if aInt > bInt {
+					return -1
+				}
+				return 0
+			} else {
+				if aInt < bInt {
+					return -1
+				} else if aInt > bInt {
+					return 1
+				}
+				return 0
+			}
+		} else {
+			if reverse {
+				return strings.Compare(vb, va)
+			}
+			return strings.Compare(va, vb)
+		}
+	})
+
+	if unique { // уникальные значния
+		lines = removeDuplicatesSorted(lines)
+	}
+
+	for _, v := range lines { //Вывод строк
+		fmt.Println(v)
+	}
 }
