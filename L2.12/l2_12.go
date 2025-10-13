24b45a410b85e56946a38a0b216fbc6855bfa2d1
mitrich772 <mitrichdmitrich772@gmail.com>
2025-10-14 02:15:50 +0300

    2.12_2

--- L2.12/l2_12.go
+++ L2.12/l2_12.go
@@ -11,11 +11,12 @@ import (
 )
 
 type Finder struct {
-	afterMatch int
-	ignoreReg  bool
+	afterMatch  int
+	beforeMatch int
+	ignoreReg   bool
 }
 
-func readLines(filename string) ([]string, error) {
+func ReadLines(filename string) ([]string, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
@@ -35,7 +36,7 @@ func readLines(filename string) ([]string, error) {
 	}
 	return lines, nil
 }
-func parseFilenameExpr() (filename string, expr string) {
+func ParseFilenameExpr() (filename string, expr string) {
 	filename = parseFilename()
 	expr = parseExpr()
 	os.Args = os.Args[:len(os.Args)-2]
@@ -53,39 +54,52 @@ func parseFilename() (filename string) {
 }
 
 // Выводит массив строк
-func printLines(lines []string) {
+func PrintLines(lines []string) {
 	for _, v := range lines {
 		fmt.Println(v)
 	}
 }
-func (fn Finder) findStrings(lines []string, template string) ([]string, error) {
+
+func (fn Finder) findWithContex(lines []string, re *regexp.Regexp) (result []string) {
+	for i, line := range lines {
+		if re.FindAllString(line, -1) != nil {
+			if (fn.beforeMatch > i) && (fn.afterMatch > (len(lines)-1)-i) { // Если в начале и конце мало строк (да так тоже может быть)
+				result = append(result, lines[:i+1]...)
+				result = append(result, lines[i:]...)
+			} else if fn.afterMatch > (len(lines)-1)-i { // Если в конце мало строк
+				result = append(result, lines[i-fn.beforeMatch:i]...) // Добавляем перед совпадением не включая совпадение
+				result = append(result, lines[i:]...)
+			} else if fn.beforeMatch > i { // Если в начале мало строк
+				result = append(result, lines[:i+1]...)
+				result = append(result, lines[i:i+fn.afterMatch+1]...) // Добавляем после совпадения включая совпадение
+			} else {
+				result = append(result, lines[i-fn.beforeMatch:i]...)  // Добавляем перед совпадением не включая совпадение
+				result = append(result, lines[i:i+fn.afterMatch+1]...) // Добавляем после совпадения включая совпадение
+			}
+
+			result = append(result, "-----------------")
+		}
+	}
+	return
+}
+func (fn Finder) FindStrings(lines []string, template string) ([]string, error) {
 	var result []string
 	// Если надо игнорировать регистр
 	if fn.ignoreReg {
 		template = `(?i)` + template
 	}
-	if fn.afterMatch > 0 {
+	// Готовим регулярку
+	re, err := regexp.Compile(template)
+	if err != nil {
+		return nil, err
+	}
+	if fn.beforeMatch > 0 || fn.afterMatch > 0 { // До или после контекст
+		result = append(result, fn.findWithContex(lines, re)...)
+	} else { //Контекст до и после равен 0 меньше нуля не может быть (Выводим только строку с совпадением)
 		re, err := regexp.Compile(template)
 		if err != nil {
 			return nil, err
 		}
-		for i, line := range lines {
-			if re.FindAllString(line, -1) != nil {
-				if fn.afterMatch > i {
-					result = append(result, lines[:i+1]...)
-				} else {
-					result = append(result, lines[i-fn.afterMatch:i+1]...)
-				}
-
-				result = append(result, "-----------------")
-			}
-		}
-	} else { //Контекст до равен 0 (Выводим только строку с совпадением)
-		re, err := regexp.Compile(template)
-		if err != nil {
-			return nil, err
-		}
-
 		for _, line := range lines {
 			if re.FindAllString(line, -1) != nil {
 				result = append(result, line)
@@ -97,25 +111,37 @@ func (fn Finder) findStrings(lines []string, template string) ([]string, error) 
 
 func main() {
 	var fn Finder
-	filename, expression := parseFilenameExpr()
+	var aroundMatch int
+	filename, expression := ParseFilenameExpr()
 
 	flag.BoolVar(&fn.ignoreReg, "i", false, "Ignore register")
 	flag.IntVar(&fn.afterMatch, "A", 0, "Print N strings after match")
 	if fn.afterMatch < 0 {
 		log.Println("-A N должно получить положительное число, так что я исправлю на ноль, типо ничего не было ;)")
 	}
+	flag.IntVar(&fn.beforeMatch, "B", 0, "Print N strings before match")
+	if fn.afterMatch < 0 {
+		log.Println("-B N должно получить положительное число, так что я исправлю на ноль, типо ничего не было ;)")
+	}
+	flag.IntVar(&aroundMatch, "C", 0, "Print N strings around match")
+	if fn.afterMatch < 0 {
+		log.Println("-A N должно получить положительное число, так что я исправлю на ноль, типо ничего не было ;)")
+	}
 	flag.Parse()
+	// Перебиваем -C в -B -A
+	fn.afterMatch = aroundMatch
+	fn.beforeMatch = aroundMatch
 
 	fmt.Printf("Имя файла: %s\nВыражение для поиска: %s\n", filename, expression)
 
-	lines, err := readLines(filename)
+	lines, err := ReadLines(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	findedLines, err := fn.findStrings(lines, expression)
+	findedLines, err := fn.FindStrings(lines, expression)
 	if err != nil {
 		log.Fatal(err)
 	}
-	printLines(findedLines)
+	PrintLines(findedLines)
 }
